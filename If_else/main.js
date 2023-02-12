6cb9416fd37cb2e27c6a255acbba16ff604eb875
hiephuynh <hiephuynh@hiephuynhs-MacBook-Pro.local>
2023-02-12 09:59:57 +0700

    12/2/2023-2

---
+++ If_else/main.js
@@ -0,0 +1,10 @@
+var date = 0;
+if(date == 2) {
+    console.log("hom nay la thu 2");
+} else if(date == 3) {
+    console.log("hom nay la thu 3");
+} else if(date == 4) {
+    console.log("hom nay la thu 4");
+} else {
+    console.log("hom nay sai");
+}
